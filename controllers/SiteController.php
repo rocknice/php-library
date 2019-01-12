ee5381f762eb31c9eeebf5e9a6f8debca26b92bc
rocknice <287866455@qq.com>
2019-01-12 22:21:44 +0800

    创建视图页面

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -63,7 +63,13 @@ class SiteController extends Controller
     {
         return $this->render('index');
     }
-
+    /**
+    新建hello页面
+    */
+    public function actionSay($message = 'Hello')
+    {
+        return $this->render('say', ['message' => $message]);
+    }
     /**
      * Login action.
      *
